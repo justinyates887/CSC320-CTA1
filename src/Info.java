91326b9efc5e8e82bd80e2f6b682404fe60941ec
Justin Yates <justinyates887@gmail.com>
2024-07-21 09:02:29 -0700

    fin

---
+++ src/Info.java
@@ -0,0 +1,26 @@
+public class Info 
+{
+	/* Justin Yates
+	 * CTA1
+	 * Option 1
+	 * 
+	 * Pseudo Code
+	 * 
+	 * This program will print out information about an individual
+	 * 
+	 * print first name
+	 * print last name
+	 * print street address
+	 * print city
+	 * print state
+	 * 
+	 */
+	public static void main(String[] args) 
+	{
+		System.out.println("First Name: Justin");
+		System.out.println("Last Name: Yates");
+		System.out.println("Street: 123 Main St.");
+		System.out.println("City: Portland");
+		System.out.println("State: OR");
+	}
+}
